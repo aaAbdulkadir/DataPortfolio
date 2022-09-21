60599f18af527af05d80fb10cdf5c12e0bd13256
aaAbdulkadir <a.abdulkadir@hotmail.com>
2022-09-21 15:50:42 +0100

    Updated README

---
+++ Portfolio/pages/5_🏀_NBA.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+# title
+st.markdown("# NBA")
+st.sidebar.markdown("# Project 4")
+
+# caption
+caption = '''
+    MAchine leanring classification and regression of NBA data.
+'''
+st.caption(caption)
